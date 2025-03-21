8ec2c45c2d904c4d8f76c5144b4403ad4487f6b5
bhavik25 <bhavik25@live.com>
2025-03-21 21:41:32 +0000

    Added first EC2

--- main.tf
+++ main.tf
@@ -12,7 +12,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create a EC2 Instance
+resource "aws_ami_copy" "example" {
+  name              = "terraform-1"
+  source_ami_id     = "ami-04b7f73ef0b798a0f"
+  source_ami_region = "us-east-1"
+
+  tags = {
+    Name = "FirstServer"
+  }
+}
+
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
