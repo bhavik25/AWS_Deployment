0035f9081f9bb05deaab73f897ff8fa22b2a11a2
bhavik25 <bhavik25@live.com>
2025-03-29 11:19:06 +0000

    moved terraform provider block to main.tf

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  cloud {
+    organization = "Terraform_space" # Replace with your HCP org
+    workspaces {
+      name = "AWS_deployment" # Replace with your workspace name
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
