ba07c542e6b79b6344eb028a39896057d3a55256
bhavik25 <bhavik25@live.com>
2025-03-29 12:46:08 +0000

    added new output variable for public ip

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -89,7 +89,6 @@ module "ec2_instance_1"{
                       echo "Script ran" > /var/log/user_data.log
                       EOF
 }
-
 module "ec2_instance_2"{
   source            = "../../modules/ec2"
   region            = "us-east-1"
@@ -117,8 +116,13 @@ module "ec2_instance_2"{
 }
 
 output "ec2_public_ip" {
-  value = module.ec2_instance.public_ip
+  value = module.ec2_instance_1.public_ip
 }
+
+output "ec2_public_ip" {
+  value = module.ec2_instance_2.public_ip
+}
+
 
 output "vpc_id" {
   value = module.vpc.vpc_id
