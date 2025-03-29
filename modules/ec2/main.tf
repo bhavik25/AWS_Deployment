ee0c7ff44d9136b486e878b5adbf46fd5e4ca125
bhavik25 <bhavik25@live.com>
2025-03-29 12:21:49 +0000

    replacing ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "ec2" {
   subnet_id     = var.subnet_id
   key_name      = var.key_name
   vpc_security_group_ids = [var.security_group_id]
-
+  user_data = var.user_data
   tags = {
     Name = var.instance_name
     Env  = var.environment
