7b1e39a2cd526d802e761db13db2065d58c9fa29
bhavik25 <bhavik25@live.com>
2025-03-29 12:49:19 +0000

    removed new server

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -92,9 +92,8 @@ module "ec2_instance"{
 
 
 output "ec2_public_ip" {
-  value = module.ec2_instance_1.public_ip
+  value = module.ec2_instance.public_ip
 }
-
 
 output "vpc_id" {
   value = module.vpc.vpc_id
